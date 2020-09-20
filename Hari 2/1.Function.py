931b48bd269ecb47c3d00fb8b44725cb9946e07e
mhafet17 <do.crazy192@gmail.com>
2020-09-20 11:56:50 +0700

    all commit

---
+++ Hari 2/1.Function.py
@@ -0,0 +1,26 @@
+# # mendefinisikan fungsi
+# def fungsi():
+#     print("ini adalah fungsi")
+#
+# # memanggil fungsi
+# fungsi()
+
+# membuat fungsi sederhana
+
+def belajar():
+    print("belajar python")
+
+def biayaBelajar():
+    belajar()
+    print("biaya belajar python adalah Rp.100000 ")
+
+# membuat fungsi dengan input argumen
+def materiPython(modul):
+    # belajar()
+    biaya = 100000
+    totalBiaya = modul * biaya
+    print('total biaya',modul , 'modul python adalah Rp.',totalBiaya)
+
+biayaBelajar()
+materiPython(5)
+
